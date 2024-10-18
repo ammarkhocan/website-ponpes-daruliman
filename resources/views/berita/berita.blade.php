cbcc7573ea8e80307eb2f958a83edc1938c75342
ammae khocan <ammarkhocan02@gmail.com>
2024-10-18 15:42:02 +0700

    Update views dan routes, add AppController Artikel Beranda

--- resources/views/berita/berita.blade.php
+++ resources/views/berita/berita.blade.php
@@ -9,43 +9,19 @@
       </div>
       
       <div class="row py-5" data-aos="zoom-in">
-        <div class="col-lg-4">
-          <div class="card border-0">
-            <img src="{{ asset('assets/images/il-berita-01.png') }}" class="img-fluid mb-3" alt="Ilustrasi Berita 1">
-            <div class="konten-berita">
-              <p class="mb-3">30/09/2024</p>
-              <h4 class="fw-bold mb-3">Pengajian Bulanan Pesantren Darul Iman</h4>
-              <p class="text-secondary">#pesantrenmodern</p>
-              <a href="#" class="text-decoration-none text-success">Selengkapnya</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="card border-0">
-            <img src="{{ asset('assets/images/il-berita-01.png') }}" class="img-fluid mb-3" alt="Ilustrasi Berita 2">
-            <div class="konten-berita">
-              <p class="mb-3">30/09/2024</p>
-              <h4 class="fw-bold mb-3">Pengajian Bulanan Pesantren Darul Iman</h4>
-              <p class="text-secondary">#pesantrenmodern</p>
-              <a href="#" class="text-decoration-none text-success">Selengkapnya</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="card border-0">
-            <img src="{{ asset('assets/images/il-berita-01.png') }}" class="img-fluid mb-3" alt="Ilustrasi Berita 3">
-            <div class="konten-berita">
-              <p class="mb-3">30/09/2024</p>
-              <h4 class="fw-bold mb-3">Pengajian Bulanan Pesantren Darul Iman</h4>
-              <p class="text-secondary">#pesantrenmodern</p>
-              <a href="#" class="text-decoration-none text-success">Selengkapnya</a>
-            </div>
-          </div>
-        </div>
-      </div>
-      
-      <div class="footer-berita text-center">
-        <a href="#" class="btn btn-outline-custom">Berita Lainnya</a>
+        @foreach ($artikels as $item)
+            <div class="col-lg-4 mb-4">
+                <div class="card border-0">
+                    <img src="{{ asset('storage/artikel/' . $item->image) }}" class="img-fluid mb-3" alt="Berita: {{ $item->judul }}">
+                    <div class="konten-berita">
+                        <p class="mb-3 text-secondary">{{ $item->created_at->format('d/m/Y') }}</p>
+                        <h4 class="fw-bold mb-3">{{ $item->judul }}</h4>
+                        <p class="text-secondary">#pesantrenmodern</p>
+                        <a href="/detail/{{ $item->slug }}" class="text-decoration-none text-success">Selengkapnya</a>
+                    </div>
+                </div>
+            </div> 
+            @endforeach
       </div>
     </div> 
   </section>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -52,198 +52,180 @@
   </section>
 
   {{-- Berita --}}
-<section id="berita" style="margin-top: 100px;">
-<div class="container">
-  <div class="header-berita text-center">
-    <h2 class="fw-bold">Berita Kegiatan Pondok Pesantren</h2>
-  </div>
-  <div class="row py-5" data-aos="zoom-in">
-    <div class="col-lg-4">
-      <div class="card border-0">
-        <img src="{{ asset('assets/images/il-berita-01.png') }}" class="img-fluid mb-3" alt="Berita 1">
-        <div class="konten-berita">
-          <p class="mb-3">30/09/2024</p>
-          <h4 class="fw-bold mb-3">Pengajian Bulanan Pesantren Darul Iman</h4>
-          <p class="text-secondary">#pesantrenmodern</p>
-          <a href="#" class="text-decoration-none text-success">Selengkapnya</a>
+  <section id="berita" style="margin-top: 100px;">
+    <div class="container">
+        <div class="header-berita text-center mb-5">
+            <h2 class="fw-bold">Berita Kegiatan Pondok Pesantren</h2>
+        </div>
+        
+        <div class="row py-5" data-aos="zoom-in">
+            @foreach ($artikels as $item)
+            <div class="col-lg-4 mb-4">
+                <div class="card border-0">
+                    <img src="{{ asset('storage/artikel/' . $item->image) }}" class="img-fluid mb-3" alt="Berita: {{ $item->judul }}">
+                    <div class="konten-berita">
+                        <p class="mb-3 text-secondary">{{ $item->created_at->format('d/m/Y') }}</p>
+                        <h4 class="fw-bold mb-3">{{ $item->judul }}</h4>
+                        <p class="text-secondary">#pesantrenmodern</p>
+                        <a href="/detail/{{ $item->slug }}" class="text-decoration-none text-success">Selengkapnya</a>
+                    </div>
+                </div>
+            </div> 
+            @endforeach
+        </div>
+
+        <div class="footer-berita text-center mt-4">
+            <a href="/berita" class="btn btn-outline-custom">Berita Lainnya</a>
+        </div>
+    </div> 
+</section>
+  {{-- Berita --}}
+
+
+  {{-- Join --}}
+  <section id="join" class="py-5">
+    <div class="container py-5">
+      <div class="row d-flex align-items-center">
+        <div class="col-lg-6" data-aos="zoom-in-right"> 
+          <div class="d-flex align-items-center mb-3">
+            <div class="stripe me-2"></div>
+            <h5>Daftar Santri</h5>
+          </div>
+          <h1 class="fw-bold mb-2">Gabung bersama kami, mewujudkan generasi rabbani</h1>
+          <p class="mb-3">
+            Pondok Pesantren Darul Iman merupakan pesantren terbaik di Lampung,
+            dengan meluluskan santri dan menjadi ustadz terkemuka mendakwahkan di pelosok nusantara.
+          </p>
+          <button class="btn btn-regis">Register</button>
+        </div>  
+        <div class="col-lg-6" data-aos="zoom-in-left">
+          <img src="{{ asset('assets/images/il-photo-04.png') }}" class="img-fluid" alt="Ilustrasi Santri">
         </div>
       </div>
     </div>
-    <div class="col-lg-4">
-      <div class="card border-0">
-        <img src="{{ asset('assets/images/il-berita-01.png') }}" class="img-fluid mb-3" alt="Berita 2">
-        <div class="konten-berita">
-          <p class="mb-3">30/09/2024</p>
-          <h4 class="fw-bold mb-3">Pengajian Bulanan Pesantren Darul Iman</h4>
-          <p class="text-secondary">#pesantrenmodern</p>
-          <a href="#" class="text-decoration-none text-success">Selengkapnya</a>
+  </section>
+  {{-- Join --}}
+
+
+  {{-- Vidio --}}
+  <section id="vidio" class="py-5">
+    <div class="container py-5" data-aos="zoom-in">
+      <div class="text-center">
+        <iframe 
+          width="560" 
+          height="315" 
+          src="https://www.youtube.com/embed/FKIn3wV8rn0?si=0RFkgM00rra910Uo" 
+          title="YouTube Video Player" 
+          frameborder="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+          referrerpolicy="strict-origin-when-cross-origin" 
+          allowfullscreen>
+        </iframe>
+      </div>
+    </div>
+  </section>
+  {{-- Vidio --}}
+
+  <section id="vidio_youtube" class="py-5" data-aos="zoom-in">
+    <div class="container">
+      <div class="header-berita text-center mb-5">
+        <h2 class="fw-bold">Video Kegiatan Pondok Pesantren</h2>
+      </div>
+      <div class="row py-5">
+        <div class="col-lg-4" data-aos="zoom-in-up">
+          <iframe 
+            width="100%" 
+            height="215" 
+            src="https://www.youtube.com/embed/FKIn3wV8rn0?si=CutsiuwqWwu4Dk5N" 
+            title="YouTube video player" 
+            frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerpolicy="strict-origin-when-cross-origin" 
+            allowfullscreen>
+          </iframe>
+        </div>
+        <div class="col-lg-4" data-aos="zoom-in-up">
+          <iframe 
+            width="100%" 
+            height="215" 
+            src="https://www.youtube.com/embed/FKIn3wV8rn0?si=CutsiuwqWwu4Dk5N" 
+            title="YouTube video player" 
+            frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerpolicy="strict-origin-when-cross-origin" 
+            allowfullscreen>
+          </iframe>
+        </div>
+        <div class="col-lg-4" data-aos="zoom-in-up">
+          <iframe 
+            width="100%" 
+            height="215" 
+            src="https://www.youtube.com/embed/FKIn3wV8rn0?si=CutsiuwqWwu4Dk5N" 
+            title="YouTube video player" 
+            frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerpolicy="strict-origin-when-cross-origin" 
+            allowfullscreen>
+          </iframe>
+        </div>
+      </div>
+      <div class="footer-berita text-center">
+        <a href="#" class="btn btn-outline-success">Berita Lainnya</a>
+      </div>
+    </div>
+  </section>
+
+  {{-- Foto --}}
+  <section id="foto" class="section-foto parallax" >
+    <div class="container" data-aos="zoom-in-up">
+      <div class="d-flex justify-content-between align-items-center mb-5">
+        <div class="d-flex align-items-center">
+          <div class="stripe-putih me-2"></div>
+          <div class="fw-bold text-white">Foto Kegiatan</div>
+        </div>
+        <div>
+          <a href="/foto" class="btn btn-outline-white">Foto Lainnya</a>
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="col-lg-3 col-md-6 col-6">
+          <a class="image-link" href="{{ asset('assets/images/il-photo-02.png') }}">
+          <img src="{{ asset('assets/images/il-photo-03.png') }}" class="img-fluid" alt="Deskripsi foto"></a>
+        </div>
+        <div class="col-lg-3 col-md-6 col-6">
+          <a class="image-link" href="{{ asset('assets/images/il-photo-02.png') }}">
+            <img src="{{ asset('assets/images/il-photo-03.png') }}" class="img-fluid" alt="Deskripsi foto">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-6 col-6">
+          <a class="image-link" href="{{ asset('assets/images/il-photo-02.png') }}">
+            <img src="{{ asset('assets/images/il-photo-03.png') }}" class="img-fluid" alt="Deskripsi foto">
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-6 col-6">
+          <a class="image-link" href="{{ asset('assets/images/il-photo-02.png') }}">
+            <img src="{{ asset('assets/images/il-photo-03.png') }}" class="img-fluid" alt="Deskripsi foto">
+          </a>
         </div>
       </div>
     </div>
-    <div class="col-lg-4">
-      <div class="card border-0">
-        <img src="{{ asset('assets/images/il-berita-01.png') }}" class="img-fluid mb-3" alt="Berita 3">
-        <div class="konten-berita">
-          <p class="mb-3">30/09/2024</p>
-          <h4 class="fw-bold mb-3">Pengajian Bulanan Pesantren Darul Iman</h4>
-          <p class="text-secondary">#pesantrenmodern</p>
-          <a href="#" class="text-decoration-none text-success">Selengkapnya</a>
-        </div>
+  </section>
+  {{-- Foto --}}
+
+  {{-- Fasilitas --}}
+  <section id="fasilitas" class="py-5" data-aos="zoom-in-up">
+    <div class="container py-5">
+      <div class="text-center mb-5">
+        <h3 class="fw-bold">Fasilitas Pesantren</h3>
+      </div>
+      <img src="{{ asset('assets/images/il-bg-video.png') }}" class="img-fluid" alt="Fasilitas Pesantren">
+      <div class="text-center mt-3">
+        <a href="#" class="btn btn-outline-success">Fasilitas Lainnya</a>
       </div>
     </div>
-  </div>
-  <div class="footer-berita text-center">
-    <a href="/berita" class="btn btn-outline-custom">Berita Lainnya</a>
-  </div>
-</div> 
-</section>
-{{-- Berita --}}
-
-
-{{-- Join --}}
-<section id="join" class="py-5">
-<div class="container py-5">
-  <div class="row d-flex align-items-center">
-    <div class="col-lg-6" data-aos="zoom-in-right"> 
-      <div class="d-flex align-items-center mb-3">
-        <div class="stripe me-2"></div>
-        <h5>Daftar Santri</h5>
-      </div>
-      <h1 class="fw-bold mb-2">Gabung bersama kami, mewujudkan generasi rabbani</h1>
-      <p class="mb-3">
-        Pondok Pesantren Darul Iman merupakan pesantren terbaik di Lampung,
-        dengan meluluskan santri dan menjadi ustadz terkemuka mendakwahkan di pelosok nusantara.
-      </p>
-      <button class="btn btn-regis">Register</button>
-    </div>  
-    <div class="col-lg-6" data-aos="zoom-in-left">
-      <img src="{{ asset('assets/images/il-photo-04.png') }}" class="img-fluid" alt="Ilustrasi Santri">
-    </div>
-  </div>
-</div>
-</section>
-{{-- Join --}}
-
-{{-- Vidio --}}
-<section id="vidio" class="py-5">
-<div class="container py-5" data-aos="zoom-in">
-  <div class="text-center">
-    <iframe 
-      width="560" 
-      height="315" 
-      src="https://www.youtube.com/embed/FKIn3wV8rn0?si=0RFkgM00rra910Uo" 
-      title="YouTube Video Player" 
-      frameborder="0" 
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-      referrerpolicy="strict-origin-when-cross-origin" 
-      allowfullscreen>
-    </iframe>
-  </div>
-</div>
-</section>
-{{-- Vidio --}}
-
-<section id="vidio_youtube" class="py-5" data-aos="zoom-in">
-<div class="container">
-  <div class="header-berita text-center mb-5">
-    <h2 class="fw-bold">Video Kegiatan Pondok Pesantren</h2>
-  </div>
-  <div class="row py-5">
-    <div class="col-lg-4" data-aos="zoom-in-up">
-      <iframe 
-        width="100%" 
-        height="215" 
-        src="https://www.youtube.com/embed/FKIn3wV8rn0?si=CutsiuwqWwu4Dk5N" 
-        title="YouTube video player" 
-        frameborder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-        referrerpolicy="strict-origin-when-cross-origin" 
-        allowfullscreen>
-      </iframe>
-    </div>
-    <div class="col-lg-4" data-aos="zoom-in-up">
-      <iframe 
-        width="100%" 
-        height="215" 
-        src="https://www.youtube.com/embed/FKIn3wV8rn0?si=CutsiuwqWwu4Dk5N" 
-        title="YouTube video player" 
-        frameborder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-        referrerpolicy="strict-origin-when-cross-origin" 
-        allowfullscreen>
-      </iframe>
-    </div>
-    <div class="col-lg-4" data-aos="zoom-in-up">
-      <iframe 
-        width="100%" 
-        height="215" 
-        src="https://www.youtube.com/embed/FKIn3wV8rn0?si=CutsiuwqWwu4Dk5N" 
-        title="YouTube video player" 
-        frameborder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-        referrerpolicy="strict-origin-when-cross-origin" 
-        allowfullscreen>
-      </iframe>
-    </div>
-  </div>
-  <div class="footer-berita text-center">
-    <a href="#" class="btn btn-outline-success">Berita Lainnya</a>
-  </div>
-</div>
-</section>
-
-{{-- Foto --}}
-<section id="foto" class="section-foto parallax" >
-<div class="container" data-aos="zoom-in-up">
-  <div class="d-flex justify-content-between align-items-center mb-5">
-    <div class="d-flex align-items-center">
-      <div class="stripe-putih me-2"></div>
-      <div class="fw-bold text-white">Foto Kegiatan</div>
-    </div>
-    <div>
-      <a href="/foto" class="btn btn-outline-white">Foto Lainnya</a>
-    </div>
-  </div>
-  
-  <div class="row">
-    <div class="col-lg-3 col-md-6 col-6">
-      <a class="image-link" href="{{ asset('assets/images/il-photo-02.png') }}">
-        <img src="{{ asset('assets/images/il-photo-03.png') }}" class="img-fluid" alt="Deskripsi foto">
-      </a>
-    </div>
-    <div class="col-lg-3 col-md-6 col-6">
-      <a class="image-link" href="{{ asset('assets/images/il-photo-02.png') }}">
-        <img src="{{ asset('assets/images/il-photo-03.png') }}" class="img-fluid" alt="Deskripsi foto">
-      </a>
-    </div>
-    <div class="col-lg-3 col-md-6 col-6">
-      <a class="image-link" href="{{ asset('assets/images/il-photo-02.png') }}">
-        <img src="{{ asset('assets/images/il-photo-03.png') }}" class="img-fluid" alt="Deskripsi foto">
-      </a>
-    </div>
-    <div class="col-lg-3 col-md-6 col-6">
-      <a class="image-link" href="{{ asset('assets/images/il-photo-02.png') }}">
-        <img src="{{ asset('assets/images/il-photo-03.png') }}" class="img-fluid" alt="Deskripsi foto">
-      </a>
-    </div>
-  </div>
-</div>
-</section>
-{{-- Foto --}}
-
-{{-- Fasilitas --}}
-<section id="fasilitas" class="py-5" data-aos="zoom-in-up">
-<div class="container py-5">
-  <div class="text-center mb-5">
-    <h3 class="fw-bold">Fasilitas Pesantren</h3>
-  </div>
-  <img src="{{ asset('assets/images/il-bg-video.png') }}" class="img-fluid" alt="Fasilitas Pesantren">
-  <div class="text-center mt-3">
-    <a href="#" class="btn btn-outline-success">Fasilitas Lainnya</a>
-  </div>
-</div>
-</section>
-{{-- Fasilitas --}}
+  </section>
+  {{-- Fasilitas --}}
 @endsection
 
    
